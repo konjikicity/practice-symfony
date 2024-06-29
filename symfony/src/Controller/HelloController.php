95bc1f948c4edb5468e8649b4f6b93e7fc5ccecc
syu.onoda <s-onoda@creative-innovation.co.jp>
2024-06-29 12:54:33 +0900

    update entity

---
+++ symfony/src/Controller/HelloController.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Person;
+use App\Form\PersonType;
+use App\Repository\PersonRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends AbstractController
+{
+    /**
+     * @var PersonRepository
+     */
+    private $personRepository;
+
+    public function __construct(PersonRepository $personRepository)
+    {
+        $this->personRepository = $personRepository;
+    }
+
+    /**
+     * @Route("/hello", name="hello")
+     */
+    public function index(Request $request): Response
+    {
+        $repository = $this->getDoctrine()
+            ->getRepository(Person::class);
+
+        $data = $repository->findall();
+
+        return $this->render('hello/index.html.twig', [
+            'title' => 'Hello',
+            'data' => $data,
+        ]);
+    }
+
+    /**
+     * @Route("/find", name="find")
+     */
+    public function find(Request $request)
+    {
+        $formobj = new FindForm();
+        $form = $this->createFormBuilder($formobj)
+            ->add('find', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Click'])
+            ->getForm();
+
+        $result = null;
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            $findstr = $form->getData()->getFind();
+
+            $result = $this->personRepository->findByAge($findstr);
+        }
+
+        return $this->render('hello/find.html.twig', [
+            'title' => 'Hello',
+            'form' => $form->createView(),
+            'data' => $result,
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     */
+    public function create(Request $request)
+    {
+        $person = new Person();
+        $form = $this->createForm(PersonType::class, $person);
+        $form->handleRequest($request);
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            $person = $form->getData();
+            $manager = $this->getDoctrine()->getManager();
+
+            $manager->persist($person);
+            $manager->flush();
+
+            return $this->redirect('/hello');
+        }
+
+        return $this->render('hello/create.html.twig', [
+            'title' => 'Hello',
+            'message' => 'Create Entity',
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/update/{id}", name="update")
+     */
+    public function update(Request $request, Person $person)
+    {
+        $form = $this->createFormBuilder($person)
+            ->add('name', TextType::class)
+            ->add('mail', TextType::class)
+            ->add('age', IntegerType::class)
+            ->add('save', SubmitType::class, ['label' => 'Click'])
+            ->getForm();
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            $person = $form->getData();
+            $manager = $this->getDoctrine()->getManager();
+
+            $manager->persist($person);
+            $manager->flush();
+
+            return $this->redirect('/hello');
+        }
+
+        return $this->render('hello/create.html.twig', [
+            'title' => 'Hello',
+            'message' => 'Update Entity id='.$person->getId(),
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete(Request $request, Person $person)
+    {
+        $form = $this->createFormBuilder($person)
+            ->add('name', TextType::class)
+            ->add('mail', TextType::class)
+            ->add('age', IntegerType::class)
+            ->add('save', SubmitType::class, ['label' => 'Click'])
+            ->getForm();
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            $person = $form->getData();
+            $manager = $this->getDoctrine()->getManager();
+
+            $manager->remove($person);
+            $manager->flush();
+
+            return $this->redirect('/hello');
+        }
+
+        return $this->render('hello/create.html.twig', [
+            'title' => 'Hello',
+            'message' => 'Delete Entity id='.$person->getId(),
+            'form' => $form->createView(),
+        ]);
+    }
+}
+
+class FindForm
+{
+    private $find;
+
+    public function getFind()
+    {
+        return $this->find;
+    }
+
+    public function setFind($find)
+    {
+        $this->find = $find;
+    }
+}
---
+++ symfony/src/Entity/Person.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PersonRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PersonRepository::class)
+ */
+class Person
+{
+    /**
+     * @ORM\Id
+     *
+     * @ORM\GeneratedValue
+     *
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $mail;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $age;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Message::class, mappedBy="person")
+     */
+    private $messages;
+
+    public function __construct()
+    {
+        $this->messages = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(?string $mail): self
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(?int $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Message>
+     */
+    public function getMessages(): Collection
+    {
+        return $this->messages;
+    }
+
+    public function addMessage(Message $message): self
+    {
+        if (! $this->messages->contains($message)) {
+            $this->messages[] = $message;
+            $message->setPerson($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMessage(Message $message): self
+    {
+        if ($this->messages->removeElement($message)) {
+            // set the owning side to null (unless already changed)
+            if ($message->getPerson() === $this) {
+                $message->setPerson(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return $this->getName();
+    }
+}
